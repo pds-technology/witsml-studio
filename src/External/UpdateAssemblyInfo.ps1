37e840320d1c8461f14b5fdf9c6422cd72c3e8a6
Bobby Diaz <bobby.diaz@pds.nl>
2016-06-03 17:20:20 -0500

    Added powershell script to update the version information in GlobalAssemblyInfo.cs during an automated build.
    Related Work Items: #5901

---
+++ src/External/UpdateAssemblyInfo.ps1
@@ -0,0 +1,15 @@
+$SourcesPath = $Env:BUILD_SOURCESDIRECTORY
+$FilePath = "$SourcesPath\src\External\GlobalAssemblyInfo.cs"
+$Content = Get-Content $FilePath
+
+$BuildNumber = $Env:BUILD_BUILDNUMBER
+$BuildParts = $BuildNumber.Split('.')
+$Major = $BuildParts[0]
+$Minor = $BuildParts[1]
+$Version = "$Major.$Minor"
+
+Write-Verbose "Replacing assembly version with build number: $BulidNumber" -Verbose
+$Content = $Content.Replace("1.0.0.0", $BuildNumber).Replace("1.0", $Version)
+
+Write-Verbose "Saving GlobalAssemblyInfo.cs to $FilePath" -Verbose
+$Content | Out-File -FilePath $FilePath
