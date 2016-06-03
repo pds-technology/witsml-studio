7c27eb250e51c96c736dae1786e43910c87584cd
Cristina Ip <cristina.ip@pds.nl>
2016-06-03 09:05:29 -0500

    Copy core libraries to all target package versions
    Related Work Items: #5884

--- src/External/CopyCoreLibrariesForDebug.bat
+++ src/External/CopyCoreLibrariesForDebug.bat
@@ -1,9 +1,17 @@
 echo Copying Core libraries for debugging...
 
-pushd "%~dp0..\packages\PDS.Framework.*\lib\net46\"
-copy "%~dp0..\..\..\witsml\src\Framework\bin\Debug\PDS.Framework.*"
-popd
+pushd "%~dp0..\packages"
 
-pushd "%~dp0..\packages\PDS.Witsml.*\lib\net46\"
-copy "%~dp0..\..\..\witsml\src\Witsml\bin\Debug\PDS.Witsml.*"
+for /D %%f in (PDS.Framework.20*) do (
+	@if exist %%f\lib\net46 (
+	    copy "%~dp0..\..\..\witsml\src\Framework\bin\Debug\PDS.Framework.*" %%f\lib\net46
+	)
+)
+
+for /D %%f in (PDS.Witsml.20*) do (
+	@if exist %%f\lib\net46 (
+	    copy "%~dp0..\..\..\witsml\src\Witsml\bin\Debug\PDS.Witsml.*" %%f\lib\net46
+	)
+)
+
 popd
