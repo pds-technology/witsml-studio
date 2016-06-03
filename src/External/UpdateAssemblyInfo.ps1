2e140f9723188bf524d5858b3484c47a8c7486a4
Bobby Diaz <bobby.diaz@pds.nl>
2016-06-03 17:31:13 -0500

    Fixed typo in powershell script.
    Related Work Items: #5901

--- src/External/UpdateAssemblyInfo.ps1
+++ src/External/UpdateAssemblyInfo.ps1
@@ -8,7 +8,7 @@ $Major = $BuildParts[0]
 $Minor = $BuildParts[1]
 $Version = "$Major.$Minor"
 
-Write-Verbose "Replacing assembly version with build number: $BulidNumber" -Verbose
+Write-Verbose "Replacing assembly version with build number: $BuildNumber" -Verbose
 $Content = $Content.Replace("1.0.0.0", $BuildNumber).Replace("1.0", $Version)
 
 Write-Verbose "Saving GlobalAssemblyInfo.cs to $FilePath" -Verbose
