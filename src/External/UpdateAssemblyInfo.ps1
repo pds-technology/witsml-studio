af8239a6f5eaf6a180b882f0778db82b32358aa4
Bobby Diaz <bobby.diaz@pds.nl>
2016-06-03 18:08:24 -0500

    Updated build number format to fix compile error.
    Related Work Items: #5901

--- src/External/UpdateAssemblyInfo.ps1
+++ src/External/UpdateAssemblyInfo.ps1
@@ -1,19 +1,12 @@
+$BuildNumber = $Env:BUILD_BUILDNUMBER
 $SourcesPath = $Env:BUILD_SOURCESDIRECTORY
+
 $FilePath = "$SourcesPath\src\External\GlobalAssemblyInfo.cs"
 $Content = Get-Content $FilePath
 
-$BuildNumber = $Env:BUILD_BUILDNUMBER
-$BuildParts = $BuildNumber.Split('.')
-$Major = $BuildParts[0]
-$Minor = $BuildParts[1]
-$Version = "$Major.$Minor.0.0"
-
 Write-Host ""
 Write-Host "Replacing assembly version with build number: $BuildNumber"
-$Content = $Content.Replace("1.0.0.0", $BuildNumber).Replace("""1.0""", """$Version""")
+$Content = $Content.Replace("1.0.0.0", $BuildNumber)
 
 Write-Host "Saving GlobalAssemblyInfo.cs to $FilePath"
 $Content | Out-File -FilePath $FilePath
-
-Write-Host ""
-Write-Host $Content
