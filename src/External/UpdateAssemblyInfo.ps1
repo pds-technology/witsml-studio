b7b5a3611e3b4960082eec06b870ea7de80419d2
Bobby Diaz <bobby.diaz@pds.nl>
2016-06-03 17:35:49 -0500

    Fixed text replace issue with double quotes.
    Related Work Items: #5901

--- src/External/UpdateAssemblyInfo.ps1
+++ src/External/UpdateAssemblyInfo.ps1
@@ -9,7 +9,7 @@ $Minor = $BuildParts[1]
 $Version = "$Major.$Minor"
 
 Write-Verbose "Replacing assembly version with build number: $BuildNumber" -Verbose
-$Content = $Content.Replace("1.0.0.0", $BuildNumber).Replace("1.0", $Version)
+$Content = $Content.Replace("""1.0.0.0""", $BuildNumber).Replace("""1.0""", $Version)
 
 Write-Verbose "Saving GlobalAssemblyInfo.cs to $FilePath" -Verbose
 $Content | Out-File -FilePath $FilePath
