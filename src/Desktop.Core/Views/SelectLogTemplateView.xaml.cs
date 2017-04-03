f23396483514fecf216cd9552ed244919cc514ad
Darshana Eeshwar <Darshana.Eeshwar@pds.nl>
2017-04-03 17:44:18 -0500

    Added dialog to select log template - "All Channels" and "Manually Enter Channels"
    Related Work Items: #9712

---
+++ src/Desktop.Core/Views/SelectLogTemplateView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PDS.WITSMLstudio.Desktop.Core.Views
+{
+    /// <summary>
+    /// Interaction logic for SelectLogTemplateView.xaml
+    /// </summary>
+    public partial class SelectLogTemplateView : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SelectLogTemplateView"/> class.
+        /// </summary>
+        public SelectLogTemplateView()
+        {
+            InitializeComponent();
+        }
+    }
+}
