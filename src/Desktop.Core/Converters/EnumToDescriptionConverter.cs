acd04f6855c60602bcb357eb088359d55b2be86f
Darshana Eeshwar <Darshana.Eeshwar@pds.nl>
2017-11-28 18:24:57 -0600

    Added enum to description converter
    Related Work Items: #17828

---
+++ src/Desktop.Core/Converters/EnumToDescriptionConverter.cs
@@ -0,0 +1,62 @@
+﻿//----------------------------------------------------------------------- 
+// PDS WITSMLstudio Desktop, 2017.2
+//
+// Copyright 2017 PDS Americas LLC
+// 
+// Licensed under the PDS Open Source WITSML Product License Agreement (the
+// "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   
+//     http://www.pds.group/WITSMLstudio/OpenSource/ProductLicenseAgreement
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-----------------------------------------------------------------------
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using PDS.WITSMLstudio.Framework;
+
+namespace PDS.WITSMLstudio.Desktop.Core.Converters
+{
+    /// <summary>
+    /// Converts an enum value to its description.
+    /// </summary>
+    public class EnumToDescriptionConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts an enum value to its description.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// A converted value. If the method returns null, the valid null value is used.
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Enum))
+                return Binding.DoNothing;
+
+            return ((Enum)value).GetDescription();
+        }
+
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
