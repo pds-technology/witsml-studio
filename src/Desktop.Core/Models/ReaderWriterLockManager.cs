022e0ef7f8fed742e774265060b57a4ac70308dd
geoffrey curtis <geoffrey.curtis@pds.nl>
2018-02-06 09:00:38 -0600

    Removed unused 'using' statements
    Related Work Items: #20524

--- src/Desktop.Core/Models/ReaderWriterLockManager.cs
+++ src/Desktop.Core/Models/ReaderWriterLockManager.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Data;
 
 namespace PDS.WITSMLstudio.Desktop.Core.Models
