eb5a2fd4f028a9b3a0de58b8c12bb961f5e2f213
Kan Yi <kan_yi@hotmail.com>
2018-10-11 12:09:40 -0500

    Make a resuable RevealablePasswordBox view and viewmodel.
    Related Work Items: #31762

---
+++ src/Desktop.Core/ViewModels/RevealablePasswordBoxViewModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using Caliburn.Micro;
+
+namespace PDS.WITSMLstudio.Desktop.Core.ViewModels
+{
+    /// <summary>
+    /// A revealable PasswordBox class 
+    /// </summary>
+    /// <seealso cref="Caliburn.Micro.Screen" />
+    public class RevealablePasswordBoxViewModel : Screen
+    {
+        private PasswordBox _passwordBox;
+
+        private string _password;
+        /// <summary>
+        /// The password
+        /// </summary>
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                if (_password == value) return;
+                _password = value;
+                if (_passwordBox != null && _passwordBox.Password != _password)
+                {
+                    _passwordBox.Password = _password;
+                }
+                NotifyOfPropertyChange(() => Password);
+            }
+        }
+
+        private bool _paswordVisible=false;
+        /// <summary>
+        /// The pasword visible
+        /// </summary>
+        public bool PasswordVisible
+        {
+            get { return _paswordVisible; }
+            set
+            {
+                if (_paswordVisible == value) return;
+                _paswordVisible = value;
+                NotifyOfPropertyChange(() => PasswordVisible);
+            }
+        }
+
+        private bool _revealable = true;
+        /// <summary>
+        /// The Revealable button visibility
+        /// </summary>
+        public bool Revealable
+        {
+            get { return _revealable; }
+            set
+            {
+                if (_revealable == value) return;
+                _revealable = value;
+                NotifyOfPropertyChange(() => Revealable);
+            }
+        }
+
+        /// <summary>
+        /// Called when [password changed].
+        /// </summary>
+        /// <param name="control">The control.</param>
+        public void OnPasswordChanged(PasswordBox control)
+        {
+            Password = control.Password;
+        }
+
+        /// <summary>
+        /// Generates the password.
+        /// </summary>
+        public void GeneratePassword()
+        {
+            Password = System.Web.Security.Membership.GeneratePassword(8, 2);
+            PasswordVisible = true;
+        }
+
+        /// <summary>
+        /// Called when a view is attached.
+        /// </summary>
+        /// <param name="view">The view.</param>
+        /// <param name="context">The context in which the view appears.</param>
+        protected override void OnViewAttached(object view, object context)
+        {
+            _passwordBox = (PasswordBox)((FrameworkElement)view).FindName("passwordBox");
+        }
+
+    }
+}
