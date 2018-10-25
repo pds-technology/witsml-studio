79ea6f5a79d11cb18d015120539fe0699d8f850a
Tedi Widjaja <tedi.widjaja@pds.nl>
2018-10-25 11:04:56 -0500

    Added FocusExtension to Desktop.Core
    Related Work Items: #31249

---
+++ src/Desktop.Core/Models/FocusExtension.cs
@@ -0,0 +1,54 @@
+﻿//----------------------------------------------------------------------- 
+// PDS WITSMLstudio StoreSync, 2018.2
+// Copyright 2018 PDS Americas LLC
+//-----------------------------------------------------------------------
+
+using System.Windows;
+
+namespace PDS.WITSMLstudio.Desktop.Core.Models
+{
+    /// <summary>
+    /// Extension method on to determine if a control is focused
+    /// </summary>
+    public static class FocusExtension
+    {
+        /// <summary>
+        /// IsFocused Property
+        /// </summary>
+        public static readonly DependencyProperty IsFocusedProperty =
+            DependencyProperty.RegisterAttached(
+                "IsFocused", typeof(bool), typeof(FocusExtension),
+                new UIPropertyMetadata(false, OnIsFocusedPropertyChanged));
+
+        /// <summary>
+        /// Gets the is focused.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns>IsFocusedProperty</returns>
+        public static bool GetIsFocused(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsFocusedProperty);
+        }
+
+        /// <summary>
+        /// Sets the is focused.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <param name="value">if set to <c>true</c> [value].</param>
+        public static void SetIsFocused(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsFocusedProperty, value);
+        }
+
+        private static void OnIsFocusedPropertyChanged(
+            DependencyObject d,
+            DependencyPropertyChangedEventArgs e)
+        {
+            var uie = (UIElement)d;
+            if ((bool)e.NewValue)
+            {
+                uie.Focus();
+            }
+        }
+    }
+}
